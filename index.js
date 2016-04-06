a53a8b6f03d67c7df1af69fb7bf9825cbc62442e
James Talmage <james@talmage.io>
2016-04-06 16:45:37 -0400

    Increase maxBuffer for sync option to the same as execa.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var child = require('child_process');
 var execa = require('execa');
+var TEN_MEBIBYTE = 1024 * 1024 * 10;
 
 module.exports = function (dir) {
 	return execa('git', ['status', '--porcelain'], {
@@ -14,7 +15,8 @@ module.exports = function (dir) {
 module.exports.sync = function (dir) {
 	var stdout = child.execFileSync('git', ['status', '--porcelain'], {
 		cwd: dir || process.cwd(),
-		encoding: 'utf8'
+		encoding: 'utf8',
+		maxBuffer: TEN_MEBIBYTE
 	});
 
 	return !(stdout && stdout.trim());
